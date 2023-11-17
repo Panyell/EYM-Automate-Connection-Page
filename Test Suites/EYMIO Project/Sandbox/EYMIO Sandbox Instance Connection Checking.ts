2be0ade82ff2a59944b0a0f6a4d1d631ff2c842f
Daniel <Daniel@DESKTOP-E3AHV49>
2023-11-17 22:35:06 +0800

    Nov.23,2023 (10:35PM)

---
+++ Test Suites/EYMIO Project/Sandbox/EYMIO Sandbox Instance Connection Checking.ts
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>EYMIO Sandbox Instance Connection Checking</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>41a02aca-8889-4c4e-b84f-bcaeb4026221</testSuiteGuid>
+   <testCaseLink>
+      <guid>4f8f77b8-9c0a-4e41-b62a-35b95249e47f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/EYM IO Sandbox Instance/Exemptax/E_Schylling-ExempTaxConnection</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>68003fc6-1636-45e2-b1ab-6d9d1dc46305</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/EYM IO Sandbox Instance/Exemptax/E_Schylling-NetsuiteTokenBasedConnection</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>146c1677-0d5b-4b11-988e-1294866e66b6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/EYM IO Sandbox Instance/Propeller/P_BambooHRConnection</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>088567ac-6edf-4951-8986-9702ec577a63</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/EYM IO Sandbox Instance/Propeller/P_NetSuiteConnection</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2bb36c88-0dea-41de-996c-a64ef783e2f5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/EYM IO Sandbox Instance/Propeller/P_SalesforceConnection</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a1063117-9855-4214-8b48-bddbd047f998</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/EYM IO Sandbox Instance/Staging/S_NetSuiteConnectionSample</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3e4acda5-441b-44c4-8d26-2da88c9a4de5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/EYM IO Sandbox Instance/Traemand/T_NetsuiteTokenBasedConnection</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0af0976f-9981-4197-a778-2dac4cb146c6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/EYM IO Sandbox Instance/Traemand/T_SalesforceTokenBasedConnection</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
